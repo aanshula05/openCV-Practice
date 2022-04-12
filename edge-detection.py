50d2dc1741d2c1d861842b4344311d8099acfab8
Aanshula Shetty <99156893+aanshula05@users.noreply.github.com>
2022-04-12 14:50:53 +0530

    Update edge-detection.py

--- edge-detection.py
+++ edge-detection.py
@@ -13,7 +13,7 @@ lap = cv.Laplacian(gray, cv.CV_64F) #cv.CV_64F is data depth
 lap = np.uint8(np.absolute(lap))
 cv.imshow('Laplacian', lap)
 
-#sobel (created gradients in x and y directions)
+#sobel (creates gradients in x and y directions)
 sobelx = cv.Sobel(gray, cv.CV_64F, 1, 0) #dx=1, dy=0
 sobely = cv.Sobel(gray, cv.CV_64F, 0, 1)
 combined_sobel = cv.bitwise_or( sobelx, sobely)
