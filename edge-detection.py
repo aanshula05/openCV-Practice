2a33c90902af34a4aa505bb269fd756df2a80fcd
Aanshula Shetty <99156893+aanshula05@users.noreply.github.com>
2022-04-12 14:48:41 +0530

    laplacian, sobel

---
+++ edge-detection.py
@@ -0,0 +1,24 @@
+import cv2 as cv
+import numpy as np
+
+img= cv.imread('Images/img1.jpg')
+cv.imshow('Image',img)
+
+#to grayscale
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+cv.imshow('Gray',gray)
+
+#laplacian(it computes the gradients of the gray img)
+lap = cv.Laplacian(gray, cv.CV_64F) #cv.CV_64F is data depth
+lap = np.uint8(np.absolute(lap))
+cv.imshow('Laplacian', lap)
+
+#sobel (created gradients in x and y directions)
+sobelx = cv.Sobel(gray, cv.CV_64F, 1, 0) #dx=1, dy=0
+sobely = cv.Sobel(gray, cv.CV_64F, 0, 1)
+combined_sobel = cv.bitwise_or( sobelx, sobely)
+
+cv.imshow('SobelX', sobelx)
+cv.imshow('SobelY', sobely)
+cv.imshow('Combined sobel', combined_sobel)
+cv.waitKey(0)
